2f4cac1c8bcfe9dbbd5ae16cccbc356a76833581
Yun Tian <sherfly.t@gmail.com>
2020-09-29 20:03:39 +0800

    Update multipleMed.R

--- multipleMed.R
+++ multipleMed.R
@@ -13,13 +13,19 @@ mw <- model_data[,2]
 
 neglinks <- model_data[,4]
 fullData <- data.frame(X = psqi, Y = mw, M1 = poslinks, M2 = neglinks)
+
 # set model
+# contrast test whether differ significantly
+# test direct effect to see whether is fully mediation
+
 multipleMediation <- '
 Y ~ b1* M1 + b2*M2 + c*X
 M1~ a1*X
 M2~ a2 * X
 indirect1 := a1*b1
 indirect2 := a2 * b2
+contrast := indirect1-indirect2 
+dirtect := c
 total := c + (a1 * b1) + (a2 * b2)
 M1 ~~ M2 
 '
