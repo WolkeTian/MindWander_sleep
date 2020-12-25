a5999c37fb4f310c114c3654e943d7efd8596369
Yun Tian <sherfly.t@gmail.com>
2020-12-25 21:07:16 +0800

    Add files via upload

---
+++ mindwander_model_covariates.R
@@ -0,0 +1,46 @@
+t0 = Sys.time()
+### model with covariates
+setwd('D:/Projects/余萍走神/睡眠和走神')
+model_data <- read.csv('modelInput_covariates.csv', header = TRUE);
+head(model_data)
+# 排列 psqi,mw, pos, neg
+
+library("lavaan")
+
+#perpration model data
+psqi <- model_data[,1]
+poslinks <- model_data[,3]
+mw <- model_data[,2]
+
+gender <- model_data[,5]
+age <- model_data[,6]
+naffect <- model_data[,7]
+rmeq <- model_data[,8]
+ess <- model_data[,9]
+
+neglinks <- model_data[,4]
+fullData <- data.frame(X = psqi, Y = mw, M1 = poslinks, M2 = neglinks, 
+                       E1 = gender, E2 = age, E3 = naffect, E4 = rmeq, E5 = ess)
+
+
+# set model
+# contrast test whether differ significantly
+# test direct effect to see whether is fully mediation
+
+multipleMediation <- '
+Y ~ b1* M1 + b2*M2 + c*X + b3*E1 + b4*E2 + b5*E3 + b6*E4 + b7*E5
+M1~ a1*X
+M2~ a2 * X
+indirect1 := a1*b1
+indirect2 := a2 * b2
+contrast := indirect1-indirect2 
+dirtect := c
+total := c + (a1 * b1) + (a2 * b2)
+M1 ~~ M2 
+'
+
+fit <- sem(model = multipleMediation, data = fullData, se = 'bootstrap', bootstrap = 10000)
+summary(fit)
+
+# see confidence interval
+parameterEstimates(fit, boot.ci.type="bca.simple")
